05affcf8fa8f8b667d195aa4da197f9d7db3af35
Sulemank-1 <suleman.dev42@gmail.com>
2026-05-18 23:19:05 +0500

    Add Person class
    This class represent a generic person

---
+++ src/Person.java
@@ -0,0 +1,32 @@
+public abstract class Person {
+    //Data Fields
+    private int id;
+    private String name;
+
+    //Constructor
+    public Person(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Methods
+    public abstract String getRole();
+}
